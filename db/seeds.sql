3b6428386105334c1a6d883b5a526a682a9fef02
Mae Kindler <mae.akie.kindler@gmail.com>
2021-01-08 15:59:38 -0600

    Adding seeds.sql file within db directory.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Peanut Butter Bacon Burger', false);
