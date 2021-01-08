b3f42191607bbb17f97f28c2f67b604c0d34392b
Mae Kindler <mae.akie.kindler@gmail.com>
2021-01-08 16:02:38 -0600

    Previous repo causing issues when trying to deploy to Heroku. Readding all code from previous work. Setting up connection.js file.

---
+++ config/connection.js
@@ -0,0 +1,36 @@
+// Set up code to connect Node to MySQL
+const mysql = require("mysql");
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "vegetable",
+        database: "burger_db"
+    });
+};
+
+connection.connect();
+
+module.exports = connection;
+
+// Code commented out below was included before setting up the JAWSDB_URL add-on.  Leaving code in file for reference.
+
+// let connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "vegetable",
+//     database: "burger_db"
+// });
+
+// connection.connect(function (err) {
+//     if (err) {
+//         console.error("Error Connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("Connected as ID " + connection.threadId);
+// });
