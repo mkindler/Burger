381aa648ca08e8bb58d040f09882e492c3c9b56e
Mae Kindler <mae.akie.kindler@gmail.com>
2021-01-08 16:05:43 -0600

    Adding burger.js file.

---
+++ models/burger.js
@@ -0,0 +1,30 @@
+// Code that will call the ORM functions using burger-specific input for the ORM
+const orm = require("../config/orm.js");
+
+const burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+
+    insertOne: function (val, cb) {
+        orm.insertOne("burgers", "burger_name", val, function (res) {
+            cb(res);
+        });
+    },
+
+    updateOne: function (boolean, condition, cb) {
+        orm.updateOne("burgers", "devoured", boolean, condition, function (res) {
+            cb(res);
+        });
+    },
+
+    deleteOne: function (condition, cb) {
+        orm.deleteOne("burgers", condition, function (res) {
+            cb(res);
+        });
+    },
+};
+
+module.exports = burger;
